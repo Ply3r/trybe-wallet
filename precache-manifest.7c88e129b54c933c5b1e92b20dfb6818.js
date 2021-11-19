357fccf151c98117d6e8b46b512943a1523b74b1
Leandro <play3rtox1c@gmail.com>
2021-11-19 16:12:06 -0300

    Updates

---
+++ precache-manifest.7c88e129b54c933c5b1e92b20dfb6818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b328f35605d7a2a594612f2f890fcd",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "65979d1e2a176832140f",
+    "url": "/trybe-wallet/static/css/main.9da12d92.chunk.css"
+  },
+  {
+    "revision": "2bd8a798a16cff2a6b64",
+    "url": "/trybe-wallet/static/js/2.b15168c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.b15168c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65979d1e2a176832140f",
+    "url": "/trybe-wallet/static/js/main.bcb9d825.chunk.js"
+  },
+  {
+    "revision": "cf2cc8bb520cf5f8882b",
+    "url": "/trybe-wallet/static/js/runtime-main.ba48076f.js"
+  },
+  {
+    "revision": "671c45aca47a3a4eda78ac0cbaf44262",
+    "url": "/trybe-wallet/static/media/background.671c45ac.jpg"
+  }
+]);
